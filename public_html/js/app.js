6b91f304faa49ea111af5a5adc8f72a6d311a219
Allen Garvey <allen.garvey@gmail.com>
2018-07-24 11:45:03 -0400

    Add ability to share memory between wasm and js

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,5 +1,15 @@
 
 WebAssembly.instantiateStreaming(fetch('js/dither.wasm'), {})
 	.then(wasmResults => {
-  		const wasmExports = wasmResults.instance.exports;
+		console.log(wasmResults);
+		const wasmExports = wasmResults.instance.exports;
+
+		//setting memory from: https://stackoverflow.com/questions/46748572/how-to-access-webassembly-linear-memory-from-c-c
+		// wasmExports.memory.grow(3);
+		const linearMemory = new Uint8Array(wasmExports.memory.buffer);
+		linearMemory[0] = 1;
+		
+		wasmExports.dither(8, 1);
+		
+		console.log(linearMemory);
 	});
--- wasm_src/main.d
+++ wasm_src/main.d
@@ -2,7 +2,12 @@
 
 extern(C): // disable D mangling
 
-double add(double a, double b) { return a + b; }
+//same as wasm instance memory
+__gshared ubyte* pixels;
+
+void dither(int imageWidth, int imageHeight){ 
+	pixels[0] = cast(ubyte)(pixels[0] + 8);
+}
 
 // seems to be the required entry point
 void _start() {}
