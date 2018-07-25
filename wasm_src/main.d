a6f3b7a79de2059f825dbd5196f5d1a29a766841
Allen Garvey <allen.garvey@gmail.com>
2018-07-25 09:39:12 -0400

    Convert dither r coefficient to template mixin

--- wasm_src/main.d
+++ wasm_src/main.d
@@ -1,6 +1,14 @@
 //main entry point
 extern(C): // disable D mangling
 
+//based on the value of r from https://en.wikipedia.org/wiki/Ordered_dithering
+//formula is hightestValue / cube_root(numColors)
+//highest value should be 1
+//r = 1 / cube_root(2) because we are using 2 colors
+mixin template DitherRCoefficient(){
+    immutable float ditherRCoefficient = 0.7937005259840997;
+}
+
 template TInitialize(T){
     //sort of halfway between static and dynamic array 
     //like dynamic array in that length and offset can be runtime values
@@ -41,11 +49,12 @@ int min(int v1, int v2, int v3){
 float pixelLightness(int r, int g, int b){
 	immutable int maxValue = max(r, g, b);
 	immutable int minValue = min(r, g, b);
-	//510 = 255 * 2
+	//510 = 255 * 2 to convert to 0-1 range
 	return (maxValue + minValue) / 510.0;
 }
 
-void fillBayerMatrix(float[] bayerMatrix, float ditherRCoefficient){
+void fillBayerMatrix(float[] bayerMatrix){
+    mixin DitherRCoefficient;
 	bayerMatrix[0] = -0.5 		 * ditherRCoefficient;
 	bayerMatrix[1] = 0.166666667 * ditherRCoefficient;
 	bayerMatrix[2] = 0.5 		 * ditherRCoefficient;
@@ -57,16 +66,9 @@ void dither(int imageWidth, int imageHeight, int heapOffset, int heapLength){
 	immutable int pixelsLength = imageWidth * imageHeight * 4;
     //pixels array starts at offset 0 in wasm heap
     ubyte[] pixels = TInitialize!(ubyte).fixedArray(0, pixelsLength);
-
-	//based on the value of r from https://en.wikipedia.org/wiki/Ordered_dithering
-    //formula is hightestValue / cube_root(numColors)
-    //highest value should be 1
-    //r = 1 / cube_root(2) because we are using 2 colors
-    immutable float ditherRCoefficient = 0.7937005259840997;
     
     //2x2 bayer matrix
     immutable int bayerDimensions = 2;
-    
     //create array using heap memory
     float[] bayerMatrix = TInitialize!(float).fixedArray(heapOffset, bayerDimensions*bayerDimensions);
 
@@ -77,9 +79,9 @@ void dither(int imageWidth, int imageHeight, int heapOffset, int heapLength){
     */
 
     //initialize matrix
-    fillBayerMatrix(bayerMatrix, ditherRCoefficient);
+    fillBayerMatrix(bayerMatrix);
     
-    //threshold where we switch between black and white
+    //lightness threshold where we switch between black and white
     immutable float threshold = 0.5;
 
     for(int pixelOffset=0,x=0,y=0;pixelOffset<pixelsLength;pixelOffset+=4){
