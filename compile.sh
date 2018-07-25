8f72b8e080d70a5bb037732851fc6246df8edcc3
Allen Garvey <allen.garvey@gmail.com>
2018-07-24 21:50:14 -0400

    Add inlining optimization to build script

--- compile.sh
+++ compile.sh
@@ -5,4 +5,5 @@
 WASM_SRC=`find ./wasm_src -type f -name '*.d'`
 #make sure '-L--no-warn-search-mismatch' is removed or commented out in /etc/ldc2.conf in the ldc2 install directory
 # have to disable array bounds check for dynamic array index
-ldc2 -mtriple=wasm32-unknown-unknown-wasm -betterC -link-internally -boundscheck=off "$WASM_SRC" -of=public_html/js/dither.wasm
+#can't use optimizations beyond inlining, or whole program gets optimized out
+ldc2 -mtriple=wasm32-unknown-unknown-wasm -betterC -link-internally -boundscheck=off -enable-inlining "$WASM_SRC" -of=public_html/js/dither.wasm -od=wasm_src
